e893dddc54876b6094eda1d6ecc0417ccd58e04a
Jose Proenca <jose@proenca.org>
2018-09-25 12:53:12 +0100

    fixed test to match new name convention

--- src/test/scala/preo/mcrl2/TestAtom.scala
+++ src/test/scala/preo/mcrl2/TestAtom.scala
@@ -8,12 +8,12 @@ class TestAtom {
   @Test
   def testAction(): Unit ={
     //assert Prints
-    assertEquals(Action("X",In(1),Some(1) ).toString , "X1in1")
-    assertEquals(Action("X", In(2), Some(1)).toString , "X1in2")
-    assertEquals(Action("X", Out(1), Some(1)).toString , "X1out1")
-    assertEquals(Action("X", Out(2), Some(1)).toString , "X1out2")
+    assertEquals(Action("X",In(1),Some(1) ).toString , "X_1i1")
+    assertEquals(Action("X", In(2), Some(1)).toString , "X_1i2")
+    assertEquals(Action("X", Out(1), Some(1)).toString , "X_1o1")
+    assertEquals(Action("X", Out(2), Some(1)).toString , "X_1o2")
     assertEquals(Action.nullAction.toString , "Null")
-    assertEquals(Action("X", Nothing, Some(1)).toString , "X1")
+    assertEquals(Action("X", Nothing, Some(1)).toString , "X_1")
     //assert Equals
     assertNotEquals(Action("X", Out(2), Some(1)), Action("X", Out(1), Some(1)))
     assertEquals(Action("Null", Nothing, None), Action.nullAction)
